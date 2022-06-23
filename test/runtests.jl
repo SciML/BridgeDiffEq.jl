fa8ce0fcfccd0d3fc085e52a4f46813344023220
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-23 15:36:29 -0400

    format SciML Style

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,28 @@
 using BridgeDiffEq, StaticArrays
 using Test
 
-α=1
-β=1
-u0=1/2
-f(u,p,t) = α*u
-g(u,p,t) = β*u
-dt = 1//2^(4)
-tspan = (0.0,1.0)
+α = 1
+β = 1
+u0 = 1 / 2
+f(u, p, t) = α * u
+g(u, p, t) = β * u
+dt = 1 // 2^(4)
+tspan = (0.0, 1.0)
 
-prob = ODEProblem(f,u0,(0.0,1.0))
-sol = solve(prob,BridgeR3(),dt=dt)
-sol = solve(prob,BridgeBS3(),dt=dt)
+prob = ODEProblem(f, u0, (0.0, 1.0))
+sol = solve(prob, BridgeR3(), dt = dt)
+sol = solve(prob, BridgeBS3(), dt = dt)
 
-prob = SDEProblem(f,g,u0,(0.0,1.0))
-@time sol = solve(prob,BridgeEuler(),dt=dt)
-sol = solve(prob,BridgeHeun(),dt=dt)
-sol = solve(prob,BridgeSRK(),dt=dt)
+prob = SDEProblem(f, g, u0, (0.0, 1.0))
+@time sol = solve(prob, BridgeEuler(), dt = dt)
+sol = solve(prob, BridgeHeun(), dt = dt)
+sol = solve(prob, BridgeSRK(), dt = dt)
 
+u0 = @SVector [2.0, 3.0]
+prob = ODEProblem(f, u0, (0.0, 1.0))
+sol = solve(prob, BridgeR3(), dt = dt)
+sol = solve(prob, BridgeBS3(), dt = dt)
 
-u0 = @SVector [2.0,3.0]
-prob = ODEProblem(f,u0,(0.0,1.0))
-sol = solve(prob,BridgeR3(),dt=dt)
-sol = solve(prob,BridgeBS3(),dt=dt)
-
-prob = SDEProblem(f,g,u0,(0.0,1.0))
-sol = solve(prob,BridgeEuler(),dt=dt)
-sol = solve(prob,BridgeHeun(),dt=dt)
+prob = SDEProblem(f, g, u0, (0.0, 1.0))
+sol = solve(prob, BridgeEuler(), dt = dt)
+sol = solve(prob, BridgeHeun(), dt = dt)
